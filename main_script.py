67f13418cae79a30c8c311ed0dfa7d45ed725f3a
Nikita Shmalko <shmalkopress@gmail.com>
2025-08-06 17:56:13 +0300

    add start virtual display

--- main_script.py
+++ main_script.py
@@ -9,10 +9,9 @@ from webdriver_manager.firefox import GeckoDriverManager
 from pyvirtualdisplay import Display
 import csv
 
-display = Display(visible=False, size=(1920, 1080))
+display = Display(visible=True, size=(1920, 1080))
 display.start()
 
-# 1. В данной функции мы проверяем, есть ли каждое поле в каждой компании(например, у Сингапура нет 'postcode'). Если поля нет, то записываем ''
 def get_text(office, locator):
     try:
         return office.find_element(*locator).text.strip()
@@ -25,9 +24,6 @@ def parse_contacts(output_file_path):
     service = Service(GeckoDriverManager().install())
     options = Options()
     arguments = ["--window-size=1920,1080",
-                 "--disable-extensions",
-                 "--disable-dev-shm-usage",
-                 "--no-sandbox",
                  "--ignore-certificate-errors"]
     for arg in arguments:
         options.add_argument(arg)
@@ -35,25 +31,19 @@ def parse_contacts(output_file_path):
     wait = WebDriverWait(driver, 10)
 
     try:
-        # 2. Открываем браузер
         driver.get("https://www.onlyoffice.com")
 
-        # 3. Ждем загрузку страницы и наводим курсор на 'Resources'
         resources_menu = wait.until(EC.element_to_be_clickable(ProjectLocators.RESOURСE_MENU))
         ActionChains(driver).move_to_element(resources_menu).perform()
 
-        # 4. Кликаем на "Contacts"
         contacts_link = wait.until(EC.element_to_be_clickable(ProjectLocators.CONTACTS_LINKS))
         contacts_link.click()
 
-        # 5. Ждем загрузку карточек компаний
         wait.until(EC.presence_of_all_elements_located(ProjectLocators.CONTACTS_INFORMATION))
         office_blocks = driver.find_elements(*ProjectLocators.CONTACTS_INFORMATION)
 
-        # 6. Создаем список, в который записываем информацию о каждой компании
         parsed_data = []
 
-        # 7. В данном цикле мы вызываем функцию get_text, проверяем наличия поля и записываем в список parsed_data
         for office in office_blocks:
             country = get_text(office, ProjectLocators.REGION_INFORMATION)
             company_name = get_text(office, ProjectLocators.COMPANIES_NAMES)
@@ -66,7 +56,6 @@ def parse_contacts(output_file_path):
 
             parsed_data.append([country, company_name, address, postal_code, phone_number])
 
-        # 8. Записываем данные в csv файл
         with open(output_file_path, 'w', newline='', encoding='utf-8') as csvfile:
             writer = csv.writer(csvfile, delimiter=';')
             writer.writerow(['Country', 'CompanyName', 'FullAddress', 'PostalCode', 'PhoneNumber'])
@@ -74,7 +63,6 @@ def parse_contacts(output_file_path):
 
         print(f"Данные сохранены в файл: {output_file_path}")
 
-        # 9. Закрываем браузер
     finally:
         driver.quit()
 
